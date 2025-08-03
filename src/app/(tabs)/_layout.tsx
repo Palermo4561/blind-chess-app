95e3f168ac81da368662bc8ae7785d61198f20e2
Grayson Palermo <Palermo.4561@gmail.com>
2025-08-03 16:19:38 -0500

    added header icons

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -13,6 +13,14 @@ const KEY_ICON_MAP: Record<IconKeyOptions, IconProp> = {
   settings: 'gear',
 }
 
+const KEY_TITLE_MAP: Record<IconKeyOptions, string> = {
+  games: 'Game Catalogue',
+  training: 'Training & Puzzles',
+  index: 'Featured Chess',
+  awards: 'Awards & Stats',
+  settings: 'Settings',
+}
+
 interface TabBarIconProps {
   color: string
   iconKey: IconKeyOptions
@@ -26,6 +34,15 @@ const TabBarIcon = ({ color, iconKey, focused, iconScale }: TabBarIconProps) => 
   return <FontAwesomeIcon icon={KEY_ICON_MAP[iconKey]} color={color} size={size} />
 }
 
+interface HeaderIconProps {
+  icon: IconProp
+  size?: number
+}
+
+const HeaderIcon = ({ icon, size = 30 }: HeaderIconProps) => {
+  return <FontAwesomeIcon color='gray' icon={icon} size={size} style={{ marginLeft: 10, marginRight: 10 }} />
+}
+
 export default function TabLayout() {
   const iconScale = {
     baseSize: 30,
@@ -37,7 +54,8 @@ export default function TabLayout() {
       screenOptions={{
         tabBarShowLabel: false,
         tabBarIconStyle: { flex: 1 },
-        headerShown: false,
+        headerLeft: () => <HeaderIcon icon='user' />,
+        headerRight: () => <HeaderIcon icon='bell' />,
       }}
     >
       {TAB_KEYS.map((iconKey, idx) => (
@@ -45,6 +63,7 @@ export default function TabLayout() {
           key={idx}
           name={iconKey}
           options={{
+            title: KEY_TITLE_MAP[iconKey],
             tabBarIcon: ({ focused, color }) => (
               <TabBarIcon iconKey={iconKey} color={color} focused={focused} iconScale={iconScale} />
             ),
